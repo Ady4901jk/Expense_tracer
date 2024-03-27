d9ed10100a8cb6bfc223cf41e351e37b17769308
Aditya <aditya4901j@gmail.com>
2024-03-28 01:40:23 +0530

    full code

--- backend/app.js
+++ backend/app.js
@@ -9,8 +9,17 @@ require('dotenv').config()
 const PORT = process.env.PORT
 
 //middlewares
+const allowedOrigins = ["http://localhost:3000", "https://expense-gkt9ibroy-adityas-projects-6d993e50.vercel.app"];
+
 app.use(cors({
-  origin: "http://localhost:3000" 
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
 }));
 app.use(express.json());
 
