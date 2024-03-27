676c8b4f817cc04baddcdc740707022d1304c327
Aditya <aditya4901j@gmail.com>
2024-03-28 01:22:36 +0530

    test6

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT
 
 //middlewares
 app.use(cors({
-  origin: " https://expense-f6nfu5ee7-adityas-projects-6d993e50.vercel.app" 
+  origin: "http://localhost:3000" 
 }));
 app.use(express.json());
 
